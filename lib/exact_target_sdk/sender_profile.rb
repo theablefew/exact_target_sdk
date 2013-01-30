b3899ecb5a0535b5a36972fd566a2263b1dfd105
Spencer Davis <spencer@promoboxx.com>
2013-01-29 20:53:22 -0500

    Add properties to SenderProfile.

--- lib/exact_target_sdk/sender_profile.rb
+++ lib/exact_target_sdk/sender_profile.rb
@@ -4,6 +4,11 @@ class SenderProfile < APIObject
   property 'CustomerKey'
   property 'Name'
   property 'Description'
+  property 'Client'
+  property 'FromAddress'
+  property 'FromName'
+  property 'SenderHeaderEmailAddress'
+  property 'SenderHeaderName'
 end
 
 end
